cc31bf0a8be71e8cddded6820a9b551e53dfff74
bogdan.bujor <bujorbogdanconstantin@gmail.com>
2021-04-19 20:07:41 +0300

    test

--- FrontEnd-Team2-main/FrontEnd-Team2-main/src/Register.js
+++ FrontEnd-Team2-main/FrontEnd-Team2-main/src/Register.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Button } from '@material-ui/core';
 import Logo from "./Logo"
 import "./styles/Register.css"
+import loginUser from "./test"
 
 const Wrapper = styled.div`
     @media only screen and (max-width : 399px) {
@@ -39,127 +40,129 @@ const BtnGoogle = styled.button`
     }
 `
 export default class Register extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          email: "",
-          username: "",
-          address: "",
-          password: "",
-          passwordConfirm: "",
-          registerErrors: ""
-        };
-    
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({
-          [event.target.name]: event.target.value
-        });
-      }
-    
-      handleSubmit(event) {
-        const { email, username, address, password, passwordConfirm } = this.state;
-      }
-    
-    render() {
-        return (
-            <div className="body">
-            <div className="center" id="logo">
-               <div>
-                 <Logo name="Helpie" />
-               </div>
-             </div>
-             <div className="center">
-               <form onSubmit={this.handleSubmit}>
-               <div className="center">
-                <label>
-                  <p>Email address</p>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </label>
-              </div>
-              <div className="center">
-                <label>
-                  <p>Username</p>
-                  <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </label>
-              </div>
-              <div className="center">
-                <label>
-                  <p>Address</p>
-                  <input
-                    type="address"
-                    name="address"
-                    placeholder="Address"
-                    value={this.state.address}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </label>
-              </div>
-              <div className="center">
-                <label>
-                  <p>Password</p>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </label>
-              </div>
-              <div className="center">
-                <label>
-                  <p>Confirm password</p>
-                  <input
-                    type="password"
-                    name="passwordConfirm"
-                    placeholder="Confirm password"
-                    value={this.state.passwordConfirm}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </label>
-              </div>
+  constructor(props) {
+    super(props);
 
-                <div className="center">
-                    <Button type="submit" variant="contained" color="primary" disableElevation>
-                        Register
-                    </Button>
-                </div>
-                <div className="center">
-                <div className="fb">
-                  <Wrapper>
-                    <BtnFacebook>
-                      &nbsp;&nbsp;Register with Facebook
-                        </BtnFacebook>
-                    <BtnGoogle>
-                      &nbsp;&nbsp;Register with Google
-                        </BtnGoogle>
-                  </Wrapper>
-                </div></div>
-                </form>
-                </div>
+    this.state = {
+      email: "",
+      username: "",
+      address: "",
+      password: "",
+      passwordConfirm: "",
+      registerErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    const { email, username, address, password, passwordConfirm } = this.state;
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <div className="center" id="logo">
+          <div>
+            <Logo name="Helpie" />
+          </div>
         </div>
-        );
-    }
+        <div className="center">
+          <form onSubmit={this.handleSubmit}>
+            <div className="center">
+              <label>
+                <p>Email address</p>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="center">
+              <label>
+                <p>Username</p>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="center">
+              <label>
+                <p>Address</p>
+                <input
+                  type="address"
+                  name="address"
+                  placeholder="Address"
+                  value={this.state.address}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="center">
+              <label>
+                <p>Password</p>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="center">
+              <label>
+                <p>Confirm password</p>
+                <input
+                  type="password"
+                  name="passwordConfirm"
+                  placeholder="Confirm password"
+                  value={this.state.passwordConfirm}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+            </div>
+
+            <div className="center">
+              <Button type="submit" variant="contained" color="primary" disableElevation onClick={() => {
+                loginUser(this.state)
+              }}>
+                Register
+                    </Button>
+            </div>
+            <div className="center">
+              <div className="fb">
+                <Wrapper>
+                  <BtnFacebook>
+                    &nbsp;&nbsp;Register with Facebook
+                        </BtnFacebook>
+                  <BtnGoogle>
+                    &nbsp;&nbsp;Register with Google
+                        </BtnGoogle>
+                </Wrapper>
+              </div></div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
