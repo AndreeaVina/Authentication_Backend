155c4657718c23d94544a39c408f1e2f8b933b62
AndreeaVina <vinaandreea27@gmail.com>
2021-05-12 00:25:27 +0300

    login with gmail in work

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
+const express = require("express");
+const app = express();
 const { checkToken } = require("../../auth/token_validation.js");
 const {
-  createUser,
-  login,
-  // getUsers,
-  updateUsers,
-  deleteUser
+    createUser,
+    login,
+    // getUsers,
+    updateUsers,
+    googleLogIn,
+    checkGmailToken,
+    logOut,
+    deleteUser
 } = require("./user.controller");
 const { getUserByEmail } = require("./user.service");
 router.post("/", checkToken, createUser);
@@ -13,4 +18,7 @@ router.post("/", checkToken, createUser);
 // router.get("/:email", getUserByEmail);
 router.post("/login", login);
 router.patch("/", updateUsers);
+router.get("/GoogleLogIn", googleLogIn);
+router.get("/logOut", logOut);
+router.post("/GoogleLogIn", checkGmailToken);
 module.exports = router;
