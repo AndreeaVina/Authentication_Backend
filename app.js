bfa9b64f2862acf474ce255fa436092061eb076f
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-05-20 14:35:40 +0300

    update header

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
 
+app.use(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+});
 app.use(express.json());
-app.use("/api/users", userRouter);
+app.use("/api/users", userRouter)
 
 
 app.listen(process.env.APP_PORT, () => {
