69f4e7ae1a11be88f0526d4fae22ed979d4bbf4b
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-05-20 14:54:45 +0300

    add cors

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const userRouter = require("./api/users/user.router");
 
-app.use(function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-});
+aap.use(cors())
 app.use(express.json());
-app.use("/api/users", userRouter)
+app.use("/api/users", userRouter);
 
 
 app.listen(process.env.APP_PORT, () => {
